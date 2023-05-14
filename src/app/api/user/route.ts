637bdc32bdc5154ac9b7ae687be87b48aa9f7c24
elluis <elluis.codes@gmail.com>
2023-05-14 05:47:00 -0500

    we are now able to send(POST) a request to make an entry in our DB! -

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,25 @@
+import prisma from "@/lib/prisma";
+import * as bcrpyt from "bcrypt";
+
+interface RequestBody {
+    name: string;
+    email: string;
+    password: string;
+}
+
+export async function POST(request: Request) {
+
+    const body:RequestBody = await request.json();
+
+    const user = await prisma.user.create({
+        data: {
+            name: body.name,
+            email: body.email,
+            password: await bcrpyt.hash(body.password, 10),
+        },
+    })
+    const { password, ...result } = user;
+    return new Response(JSON.stringify(result));
+
+
+}
