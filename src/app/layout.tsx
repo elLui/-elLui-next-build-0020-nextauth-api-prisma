3ad42f0eaafdaeadcde2a33c30a44fd04948e818
elluis <elluis.codes@gmail.com>
2023-05-14 02:36:27 -0500

    just wrapped our rootLayout with next-auth SessionProvider -
    everything working so far -

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Provider from "@/components/auth/provider";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+    <Provider>
       <body className={inter.className}>{children}</body>
+    </Provider>
     </html>
   )
 }
